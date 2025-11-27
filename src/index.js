26292e62a4850a0c548e54c1d30e6c0ffe2173eb
Osmar Bruno <brunoosmar442@gmail.com>
2025-11-27 18:39:19 -0300

    Desligando funcionamento do mongodb

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import express from 'express'
 import cors from 'cors'
 import routes from './routes.js' // Garanta que routes.js está na mesma pasta (src)
 import { config } from 'dotenv'
-import connect_MongoDB from './database/mongoDB.js' // Aponta para src/database
+//import connect_MongoDB from './database/mongoDB.js' // Aponta para src/database
 
 config()
 
 const app = express()
 
 // Conexão sem await no topo
-connect_MongoDB()
-    .then(() => console.log("Mongo Conectado"))
-    .catch(err => console.error(err));
+//connect_MongoDB()
+//    .then(() => console.log("Mongo Conectado"))
+//    .catch(err => console.error(err));
 
 app.use(express.json())
 app.use(cors())
